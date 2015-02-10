f3ccdc0cc4ef00c23953404a585f80eb20fcd569
rac <rac@Shrin-G.local>
2015-02-10 15:30:06 +0530

    Initial Commit

---
+++ AFStore/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  AFStore
+//
+//  Created by rac on 10/02/15.
+//  Copyright (c) 2015 ___SANDS_TECHNOLOGIES___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
